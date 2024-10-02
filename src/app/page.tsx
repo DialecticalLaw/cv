75047d1d3edfa6fcc6526bbab9daa1f95720ec92
DialecticalLaw <d.shmuratkin@mail.ru>
2024-10-02 22:16:53 +0300

    feat: add summary section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Contacts } from '@/components/Contacts/Contacts';
 import styles from './page.module.css';
+import { Summary } from '@/components/Summary/Summary';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Contacts />
+      <Summary />
     </main>
   );
 }
---
+++ src/components/Summary/Summary.tsx
@@ -0,0 +1,16 @@
+import styles from './Summary.module.css';
+
+export function Summary() {
+  return (
+    <section className={styles.section}>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis odio dolores obcaecati voluptate nulla ut
+        totam nobis aliquam consequatur ullam officia necessitatibus dolorum a quos atque asperiores nam assumenda error
+        iste, eligendi saepe est tenetur enim. Dolore nemo assumenda soluta? Sed ab eum nesciunt sapiente! Aut ipsum
+        provident harum quasi quos ratione quis mollitia tempora cupiditate? Officiis consequatur nostrum inventore
+        culpa deleniti, eligendi corrupti esse beatae quisquam, aliquam quo voluptatibus exercitationem aspernatur sit
+        dolore delectus voluptate eum maxime iure itaque.
+      </p>
+    </section>
+  );
+}
