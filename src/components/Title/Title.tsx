671e42c6c3616d18f126fd161e7ff45eb3beda98
DialecticalLaw <d.shmuratkin@mail.ru>
2024-10-03 22:36:48 +0300

    feat: improve design of the Title

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,5 +1,11 @@
 import styles from './Title.module.css';
 
 export function Title({ children }: { children: React.ReactNode }) {
-  return <h2 className={styles.title}>{children}</h2>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.line} />
+      <h2 className={styles.title}>{children}</h2>
+      <div className={styles.line} />
+    </div>
+  );
 }
