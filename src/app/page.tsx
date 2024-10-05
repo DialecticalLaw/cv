122f507475ac75d52bddec55c2be2d09d64d6d84
DialecticalLaw <d.shmuratkin@mail.ru>
2024-10-05 21:59:03 +0300

    feat: add intro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,22 @@ import styles from './page.module.css';
 import { Summary } from '@/components/Summary/Summary';
 import { Education } from '@/components/Education/Education';
 import { Languages } from '@/components/Languages/Languages';
+import locationIcon from '@/assets/img/location.svg';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <div className={styles.intro}>
+        <p className={styles.text_name}>Денис Шмураткин</p>
+        <div className={styles.image}>
+          <div className={styles.glass} />
+        </div>
+        <p className={styles.text_location}>
+          <Image className={styles.icon} src={locationIcon} alt="location" /> Локация: Россия, Сыктывкар
+        </p>
+      </div>
+
       <div className={styles.contacts_summary}>
         <Contacts />
         <Summary />
