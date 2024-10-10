98cb4ddeea949a53dad74b05c9e4c07491650bcc
DialecticalLaw <d.shmuratkin@mail.ru>
2024-10-10 22:11:47 +0300

    feat: update summary

--- src/components/Summary/Summary.tsx
+++ src/components/Summary/Summary.tsx
@@ -6,12 +6,8 @@ export function Summary() {
     <section className={styles.section}>
       <Title>Обо мне</Title>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis odio dolores obcaecati voluptate nulla ut
-        totam nobis aliquam consequatur ullam officia necessitatibus dolorum a quos atque asperiores nam assumenda error
-        iste, eligendi saepe est tenetur enim. Dolore nemo assumenda soluta? Sed ab eum nesciunt sapiente! Aut ipsum
-        provident harum quasi quos ratione quis mollitia tempora cupiditate? Officiis consequatur nostrum inventore
-        culpa deleniti, eligendi corrupti esse beatae quisquam, aliquam quo voluptatibus exercitationem aspernatur sit
-        dolore delectus voluptate eum maxime iure itaque.
+        Фронтенд-разработчик, стремящийся найти баланс между функциональностью и эстетикой, создавая интуитивно понятные
+        и вдохновляющие пользовательские интерфейсы.
       </p>
     </section>
   );
