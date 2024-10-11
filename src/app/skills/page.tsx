6a0b1c42d22c49852ca94ed168ba621a6e2c6499
DialecticalLaw <d.shmuratkin@mail.ru>
2024-10-11 16:53:11 +0300

    fix: correct titles

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,11 +1,10 @@
 import { HardSkills } from '@/components/HardSkills/HardSkills';
-import styles from './page.module.css';
 import { SoftSkills } from '@/components/SoftSkills/SoftSkills';
 
 export default function Skills() {
   return (
     <>
-      <h1 className={styles.title}>Навыки</h1>
+      <h1>Навыки</h1>
       <HardSkills />
       <SoftSkills />
     </>
