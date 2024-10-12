72510653cd68fcd2bd1f3c5207ed70c00ee91a7e
DialecticalLaw <d.shmuratkin@mail.ru>
2024-10-12 23:56:27 +0300

    feat: add projects

--- src/helpers/projects.tsx
+++ src/helpers/projects.tsx
@@ -1,22 +1,62 @@
 export const projects = [
   {
     title: 'Library',
-    desc: 'Landing сайта по подбору и продаже книг',
-    link: 'https://video.mp4',
+    desc: 'Landing библиотеки',
+    link: 'https://www.dropbox.com/scl/fi/5ksoqtu4fso3mlinpg3xv/library.mp4?rlkey=akj6coy2n5lxs9sqn0a85xp6k&st=01tzh0vv&dl=0&raw=1',
   },
   {
     title: 'Image Gallery',
-    desc: 'Поиск изображений по API',
-    link: 'https://video.mp4',
+    desc: 'Поиск изображений через API',
+    link: 'https://www.dropbox.com/scl/fi/jmz8n4p08obuizu504uqa/image_gallery.mp4?rlkey=yr19hco87b8f7unwp1xu5n69g&st=34bgeeps&dl=0&raw=1',
   },
   {
     title: 'Audio player',
     desc: 'Музыкальный плеер',
-    link: 'https://video.mp4',
+    link: 'https://www.dropbox.com/scl/fi/txpwmyx07jyutuxymbj6u/audio_player.mp4?rlkey=o2jopic3827jtkpbsgtftjohb&st=dwwlzx6d&dl=0&raw=1',
   },
   {
     title: '2048',
     desc: 'Игра по слиянию чисел',
-    link: 'https://video.mp4',
+    link: 'https://www.dropbox.com/scl/fi/ynjhcpmc0lnqt897cgwvi/2048.mp4?rlkey=pgi1kbq6xeqqm8wk6auywa60q&st=6hmz6b9q&dl=0&raw=1',
+  },
+  {
+    title: 'Coffee House',
+    desc: 'Уютный Landing кафе',
+    link: 'https://www.dropbox.com/scl/fi/occ036xrz6imwh6t1h247/coffee_house.mp4?rlkey=8nld46e9nowrfvzi528t3ld25&st=7fk3d85e&dl=0&raw=1',
+  },
+  {
+    title: 'Hangman',
+    desc: 'Игра "Виселица"',
+    link: 'https://www.dropbox.com/scl/fi/vg5eycovq7zhy2bhyr1bz/hangman.mp4?rlkey=2yiain3xkus6f3pchxcs5u9rx&st=sivbzzrm&dl=0&raw=1',
+  },
+  {
+    title: 'Nonograms',
+    desc: 'Японская головоломка',
+    link: 'https://www.dropbox.com/scl/fi/m4tdu7wn5nypjtjx8klse/nonograms.mp4?rlkey=5t6wvaj07ey1plqa0e94fc06h&st=fn4vqzdb&dl=0&raw=1',
+  },
+  {
+    title: 'Async Race',
+    desc: 'Управление коллекцией машин',
+    link: 'https://www.dropbox.com/scl/fi/ggb0g0kdqe0e4q72wf768/async_race.mp4?rlkey=uysiwjyoxiuip6c03kqo0oqsg&st=2igz7t56&dl=0&raw=1',
+  },
+  {
+    title: 'Puzzle',
+    desc: 'Интерактивное изучение английского',
+    link: 'https://www.dropbox.com/scl/fi/py0cu0x5nvvicxw990tow/puzzle.mp4?rlkey=vg7y649zzuuvh4cp6omj6oko8&st=llh1ch6c&dl=0&raw=1',
+  },
+  {
+    title: 'Chat',
+    desc: 'Чат. Просто чат.',
+    link: 'https://www.dropbox.com/scl/fi/lq4sou0g9oa6ruu2tx0lb/chat.mp4?rlkey=u2vfwlyg6c0tbfbpv1z6s870a&st=h5di0d76&dl=0&raw=1',
+  },
+  {
+    title: "Sloths's Galactic Store",
+    desc: 'Межгалактический интернет-магазин',
+    link: 'https://www.dropbox.com/scl/fi/1swe7286q23555kw77ujp/sloths_galactic_store.mp4?rlkey=7teq6mywjecdwt7uhy6fj2jvh&st=3re2ti87&dl=0&raw=1',
+  },
+  {
+    title: 'CV',
+    desc: 'Портфолио. Вы уже на нём :)',
+    link: '',
   },
 ];
