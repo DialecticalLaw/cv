da73045fac4fc09a943f1313252bfdd9b7260241
DialecticalLaw <d.shmuratkin@mail.ru>
2024-10-12 13:13:49 +0300

    feat: add a list of projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,3 +1,19 @@
+import styles from './page.module.css';
+import { projects } from '@/helpers/projects';
+
 export default function Projects() {
-  return <h1>Проекты</h1>;
+  return (
+    <section>
+      <h1>Проекты</h1>
+      <div className={styles.wrapper}>
+        {projects.map((project) => (
+          <div className={styles.project} key={project.title}>
+            <p className={styles.title}>{project.title}</p>
+            <p className={styles.desc}>{project.desc}</p>
+            <p className={styles.hint}>Подробнее &rarr;</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
---
+++ src/helpers/projects.tsx
@@ -0,0 +1,22 @@
+export const projects = [
+  {
+    title: 'Library',
+    desc: 'Landing сайта по подбору и продаже книг',
+    link: 'https://video.mp4',
+  },
+  {
+    title: 'Image Gallery',
+    desc: 'Поиск изображений по API',
+    link: 'https://video.mp4',
+  },
+  {
+    title: 'Audio player',
+    desc: 'Музыкальный плеер',
+    link: 'https://video.mp4',
+  },
+  {
+    title: '2048',
+    desc: 'Игра по слиянию чисел',
+    link: 'https://video.mp4',
+  },
+];
