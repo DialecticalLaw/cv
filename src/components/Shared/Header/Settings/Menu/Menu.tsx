9d2698623fa407142de3826e60b2d534efdfbaec
DialecticalLaw <d.shmuratkin@mail.ru>
2024-10-17 14:36:47 +0300

    feat: implement opening and closing the settings menu

--- src/components/Shared/Header/Settings/Menu/Menu.tsx
+++ src/components/Shared/Header/Settings/Menu/Menu.tsx
@@ -1,20 +1,26 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styles from './Menu.module.css';
 
 type Language = 'russian' | 'english';
 
-export function Menu() {
+export function Menu({ isOpen, setOpen }: { isOpen: boolean; setOpen: React.Dispatch<React.SetStateAction<boolean>> }) {
   const [language, setLanguage] = useState<Language>('russian');
   const [theme, setTheme] = useState<'dark' | 'light'>('light');
+  const menuRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    console.log(theme);
-  }, [theme]);
+    const handleClick = (e: MouseEvent) => {
+      if (!isOpen) return;
+      if (!menuRef.current?.contains(e.target as HTMLElement | null)) setOpen(false);
+    };
+    document.addEventListener('click', handleClick);
+    return () => document.removeEventListener('click', handleClick);
+  }, [isOpen, setOpen]);
 
   return (
-    <div className={styles.wrapper}>
+    <div ref={menuRef} className={`${styles.wrapper} ${isOpen ? styles.visible : ''}`}>
       <select
         name="language"
         value={language}
--- src/components/Shared/Header/Settings/Settings.tsx
+++ src/components/Shared/Header/Settings/Settings.tsx
@@ -1,16 +1,21 @@
+'use client';
+
 import Image from 'next/image';
 import styles from './Settings.module.css';
 import settingsIcon from '@/assets/img/settings.svg';
 import { Menu } from './Menu/Menu';
+import { useState } from 'react';
 
 export function Settings() {
+  const [isOpen, setOpen] = useState(false);
+
   return (
     <div className={styles.wrapper}>
-      <button className={styles.settings}>
+      <button className={`${styles.settings} ${isOpen ? styles.open : ''}`} onClick={() => setOpen((prev) => !prev)}>
         <Image priority className={styles.icon} src={settingsIcon} alt="settings" />
       </button>
 
-      <Menu />
+      <Menu isOpen={isOpen} setOpen={setOpen} />
     </div>
   );
 }
