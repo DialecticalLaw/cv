633c95b398c98dbdcb8ec91d8531556ad2daa3cc
DialecticalLaw <d.shmuratkin@mail.ru>
2024-10-25 22:33:36 +0300

    feat: add closure to the burger menu when clicked outside of it

--- src/components/Shared/Header/Header.tsx
+++ src/components/Shared/Header/Header.tsx
@@ -1,16 +1,26 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styles from './Header.module.css';
 import { NavLink } from './NavLink/NavLink';
 import { Settings } from './Settings/Settings';
 
 export function Header() {
   const [isNavOpen, setNavOpen] = useState(false);
+  const navRef = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    const handleClick = (e: MouseEvent) => {
+      if (!isNavOpen) return;
+      if (!navRef.current?.contains(e.target as HTMLElement | null)) setNavOpen(false);
+    };
+    document.addEventListener('click', handleClick);
+    return () => document.removeEventListener('click', handleClick);
+  }, [isNavOpen]);
 
   return (
     <header className={styles.header}>
-      <nav className={`${styles.nav} ${isNavOpen ? styles.active : ''}`}>
+      <nav ref={navRef} className={`${styles.nav} ${isNavOpen ? styles.active : ''}`}>
         <ul className={styles.nav_list}>
           <NavLink href="/">summary</NavLink>
           <NavLink href="/skills">skills</NavLink>
